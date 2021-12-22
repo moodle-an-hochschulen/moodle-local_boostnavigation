b9e5bcbfee9f4c0f8a11c0e069d37a5cd57361f7
Alexander Bias <bias@alexanderbias.de>
2021-12-22 21:17:33 +0100

    Fix JSDoc flaw which used the @Package tag in the wrong way

--- amd/src/collapsenavdrawernodes.js
+++ amd/src/collapsenavdrawernodes.js
@@ -16,7 +16,7 @@
 /**
  * Local plugin "Boost navigation fumbling" - JS code for collapsing nav drawer nodes
  *
- * @package    local_boostnavigation
+ * @module     local_boostnavigation/collapsenavdrawernodes
  * @copyright  2017 Alexander Bias, Ulm University <alexander.bias@uni-ulm.de>
  * @copyright  2017 Kathrin Osswald, Ulm University <kathrin.osswald@uni-ulm.de>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
